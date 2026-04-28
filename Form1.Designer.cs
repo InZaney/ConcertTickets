f69c7a846841d845e8fbf0d648d2539f97fd031c
Zane Marcoe <zmarcoe5840@students.cptc.edu>
2026-04-28 09:37:01 -0700

    Created README.md and form layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblType = new Label();
+            lblAmount = new Label();
+            lblTotal = new Label();
+            btnCheckout = new Button();
+            cboxType = new ComboBox();
+            numAmount = new NumericUpDown();
+            txtTotal = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)numAmount).BeginInit();
+            SuspendLayout();
+            // 
+            // lblType
+            // 
+            lblType.AutoSize = true;
+            lblType.Location = new Point(4, 80);
+            lblType.Name = "lblType";
+            lblType.Size = new Size(67, 15);
+            lblType.TabIndex = 0;
+            lblType.Text = "Ticket Type";
+            // 
+            // lblAmount
+            // 
+            lblAmount.AutoSize = true;
+            lblAmount.Location = new Point(20, 154);
+            lblAmount.Name = "lblAmount";
+            lblAmount.Size = new Size(51, 15);
+            lblAmount.TabIndex = 1;
+            lblAmount.Text = "Amount";
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.Location = new Point(33, 230);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(33, 15);
+            lblTotal.TabIndex = 2;
+            lblTotal.Text = "Total";
+            // 
+            // btnCheckout
+            // 
+            btnCheckout.Location = new Point(45, 295);
+            btnCheckout.Name = "btnCheckout";
+            btnCheckout.Size = new Size(149, 72);
+            btnCheckout.TabIndex = 3;
+            btnCheckout.Text = "Checkout";
+            btnCheckout.UseVisualStyleBackColor = true;
+            // 
+            // cboxType
+            // 
+            cboxType.FormattingEnabled = true;
+            cboxType.Location = new Point(77, 77);
+            cboxType.MaxDropDownItems = 3;
+            cboxType.Name = "cboxType";
+            cboxType.Size = new Size(82, 23);
+            cboxType.TabIndex = 4;
+            // 
+            // numAmount
+            // 
+            numAmount.Location = new Point(78, 152);
+            numAmount.Name = "numAmount";
+            numAmount.Size = new Size(45, 23);
+            numAmount.TabIndex = 5;
+            // 
+            // txtTotal
+            // 
+            txtTotal.Location = new Point(78, 227);
+            txtTotal.Name = "txtTotal";
+            txtTotal.ReadOnly = true;
+            txtTotal.Size = new Size(81, 23);
+            txtTotal.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(275, 450);
+            Controls.Add(txtTotal);
+            Controls.Add(numAmount);
+            Controls.Add(cboxType);
+            Controls.Add(btnCheckout);
+            Controls.Add(lblTotal);
+            Controls.Add(lblAmount);
+            Controls.Add(lblType);
+            Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numAmount).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblType;
+        private Label lblAmount;
+        private Label lblTotal;
+        private Button btnCheckout;
+        private ComboBox cboxType;
+        private NumericUpDown numAmount;
+        private TextBox txtTotal;
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# ConcertTickets
+# Concert Tickets
+This form will let you fill out what type of concert ticket you want (including price), the amount of tickets you want and will automatically
+calculate how much your total cost is.  
+NOTE: these prices will not be accurate or based on a specific concert
+
+## Requirements
+- .Net 10
+- Windows 10/11
+- (Prefered) Visual Studio
+
+## Screenshot
+
+
